95dc4f8484230f040291f326d4e989b46910ede2
Guaitoline <92082869+Guaitoline@users.noreply.github.com>
2021-10-09 00:01:46 +0200

    Add files via upload

---
+++ Hola Mundo.java
@@ -0,0 +1,10 @@
+NombreArchivo: "HelloWorld.java". */
+class HelloWorld
+{
+    // Tu programa comienza con una llamada a main().
+    // Imprime "Hola Mundo" a la ventana de la terminal.
+    public static void main(String args[])
+    {
+        System.out.println("¡Ola Mundoo!");
+    }
+}
